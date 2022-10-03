a21d8d443127896678f45b0f4549dd1ec1be6fa5
Susmi Benny <89653115+sbenny1230@users.noreply.github.com>
2022-10-03 22:22:12 +0100

    Show bmi calculator in main page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BmiCalculatorComponent } from './bmi-calculator/bmi-calculator.component';
-import { BmiCalculatorAdultComponent } from './bmi-calculator-adult/bmi-calculator-adult.component';
-import { BmiCalculatorChildComponent } from './bmi-calculator-child/bmi-calculator-child.component';
+import { BmiCalculatorAdultComponent } from './bmi-calculator/bmi-calculator-adult/bmi-calculator-adult.component';
+import { BmiCalculatorChildComponent } from './bmi-calculator/bmi-calculator-child/bmi-calculator-child.component';
 
 @NgModule({
   declarations: [
