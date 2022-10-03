c2ac0fa36c9fa225ad4fcb6cd8dc66fb3673e33c
Susmi Benny <89653115+sbenny1230@users.noreply.github.com>
2022-10-03 21:59:05 +0100

    Add initial bmi-calculator component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BmiCalculatorComponent } from './bmi-calculator/bmi-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BmiCalculatorComponent
   ],
   imports: [
     BrowserModule,
