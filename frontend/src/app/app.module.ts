d7cde04d44efd6d2881999240bb7dd2b301c2ca8
Susmi Benny <89653115+sbenny1230@users.noreply.github.com>
2022-10-17 19:37:40 +0100

    Remove bmi calculator from app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BmiCalculatorComponent } from './bmi-calculator/bmi-calculator.component';
 import { CreatinineCalculatorComponent } from './creatinine-calculator/creatinine-calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BmiCalculatorComponent,
     CreatinineCalculatorComponent
   ],
   imports: [
