98355264d1ac274520516aeaec3f0f90bbbdbea2
Susmi Benny <89653115+sbenny1230@users.noreply.github.com>
2022-10-27 21:19:28 +0100

    Create template driven creatinine form

--- frontend/src/app/creatinine-calculator/creatinine-calculator.component.ts
+++ frontend/src/app/creatinine-calculator/creatinine-calculator.component.ts
@@ -1,28 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-creatinine-calculator',
   templateUrl: './creatinine-calculator.component.html',
   styleUrls: ['./creatinine-calculator.component.css']
 })
+
 export class CreatinineCalculatorComponent implements OnInit {
+  weight = 0;
+  creatinine = 0;
 
-  ccForm = this.formBuilder.group({
-    age: '',
-    weight: '',
-    creatinine: ''
-  });
-
-  constructor(
-    private formBuilder: FormBuilder
-    ) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    console.log(this.ccForm.value)
   }
-
 }
